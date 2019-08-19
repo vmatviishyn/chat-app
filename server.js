36fac6c980ab3ec9ba1487f3f48e77e57372d50c
Vitalii Matviishyn <vmatviishyn@gmail.com>
2019-08-19 22:14:47 +0300

    test ionic cors

--- server.js
+++ server.js
@@ -7,7 +7,32 @@ const path = require('path');
 
 const app = express();
 
+const allowedOrigins = [
+  'capacitor://localhost',
+  'ionic://localhost',
+  'http://localhost',
+  'http://localhost:8080',
+  'http://localhost:8100'
+];
+
 app.use(cors());
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  }
+}
+
+app.options('*', cors(corsOptions));
+
+app.get('/', cors(corsOptions), (req, res, next) => {
+  res.json({ message: 'This route is CORS-enabled for an allowed origin.' });
+})
+
 
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
